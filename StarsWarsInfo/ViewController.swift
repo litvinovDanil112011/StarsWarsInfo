23dcc35fc229f6fa1ef905dd9bc22eec13cfb358
litvinovDanil112011 <danil112011@mail.ru>
2025-02-11 11:58:27 +0300

    Initial Commit

---
+++ StarsWarsInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarsWarsInfo
+//
+//  Created by Daniil Litvinov on 11.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
