d2179351ab889bdd5b38e6b99a52763513c7199a
litvinovDanil112011 <danil112011@mail.ru>
2025-02-11 12:17:21 +0300

    add-struck-project.
    Подготовил проект для работы

---
+++ StarsWarsInfo/Moduls/MainScreen/MainViewController/MainViewConroller.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewConroller.swift
+//  StarsWarsInfo
+//
+//  Created by Daniil Litvinov on 11.02.2025.
+//
+
+import UIKit
+
+final class MainViewConroller: UIViewController {
+    
+    //MARK: - Life Cicle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupHerarcy()
+        setupLayuot()
+        setupViews()
+    }
+    //MARK: - Setups
+    private func setupHerarcy() {
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .red
+    }
+    
+    private func setupLayuot() {
+    }
+    
+}
