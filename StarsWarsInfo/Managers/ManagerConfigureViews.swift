a30fe2fa5a492d97de6531f0ecc25b44a6fec8cb
litvinovDanil112011 <danil112011@mail.ru>
2025-02-11 17:35:08 +0300

    add-Button.

--- StarsWarsInfo/Managers/ManagerConfigureViews.swift
+++ StarsWarsInfo/Managers/ManagerConfigureViews.swift
@@ -10,17 +10,28 @@ import UIKit
 class ManagerConfigureViews {
     
     static let shared = ManagerConfigureViews()
+    let fontSW = UIFont(name: "StarJediOutline-y0xm", size: 30.0)
     
     //MARK: Label
-    func configureLabel(title: String) -> UILabel {
+    public func configureLabel(title: String) -> UILabel {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .white
         label.textAlignment = .center
         label.text = title
-        label.numberOfLines = 2
-        label.font = .systemFont(ofSize: 50, weight: .bold)
+        label.numberOfLines = 3
+        label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
         return label
+    }
+    
+    public func configureButton(title: String, colorBackground: UIColor) -> UIButton {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(colorBackground, for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .white
+        return button
     }
     
 }
--- StarsWarsInfo/Moduls/OnboardingScreen/OnbordingViewController/OnbordingViewController.swift
+++ StarsWarsInfo/Moduls/OnboardingScreen/OnbordingViewController/OnbordingViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 final class OnbordingViewController: UIViewController {
     //MARK: Var
     let backroundImage = UIImageView(image: UIImage(named: "skyScreenSaven"))
-    let labelStarWars = ManagerConfigureViews.shared.configureLabel(title: "Stars Wars")
+    let labelStarWars = ManagerConfigureViews.shared.configureLabel(title: "Stars Wars Info")
+    let pushMainScreen = ManagerConfigureViews.shared.configureButton(title: "Start", colorBackground: .blue)
     //MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHerarcy()
         setupViews()
         setupLayuot()
-    }
-    
+    }    
     //MARK: - Setups
     private func setupHerarcy() {
         view.addSubview(backroundImage)
         view.sendSubviewToBack(backroundImage)
         view.addSubview(labelStarWars)
+        view.addSubview(pushMainScreen)
     }
     
     private func setupViews() {
@@ -41,6 +42,11 @@ final class OnbordingViewController: UIViewController {
             labelStarWars.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             labelStarWars.widthAnchor.constraint(equalToConstant: 200),
             labelStarWars.heightAnchor.constraint(equalToConstant: 200),
+            
+            pushMainScreen.trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor, constant: -20),
+            pushMainScreen.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: -20),
+            pushMainScreen.widthAnchor.constraint(equalToConstant: 100),
+            pushMainScreen.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
     
